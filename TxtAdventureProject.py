d379accf2c5211045ed07ec299786d89d90e897a
voorXIII <demarkisgeorge1@gmail.com>
2019-04-04 17:25:05 -0400

    Added classes

--- TxtAdventureProject.py
+++ TxtAdventureProject.py
@@ -1,8 +1,10 @@
 print("Welcome to the game")
 
-name = input("Choose your name")
+name = input("Choose your name\n")
 print(name)
 
-user_class = input("Choose a class")
+user_class = input("Choose a class between Warrior, Mage, or Rouge\n")
 print(user_class)
-
+while(user_class is not 'Warrior' or user_class is not 'Mage' or user_class is not 'Rouge'):
+    print("You did not choose from the required classes, try again")
+    user_class = input()
