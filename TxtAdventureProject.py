dd779f5d56d050c8019e2e107d0774c7446ea4b7
voorXIII <48102563+voorXIII@users.noreply.github.com>
2019-04-04 18:19:27 -0400

    Update TxtAdventureProject.py

--- TxtAdventureProject.py
+++ TxtAdventureProject.py
@@ -3,8 +3,26 @@ print("Welcome to the game")
 name = input("Choose your name\n")
 print(name)
 
+class Warrior:
+    HP = 200
+    ATK = 300
+    DEF = 150
+
+class Mage:
+    HP = 100
+    ATK = 200
+    DEF = 0
+
+class Rouge:
+    HP = 150
+    ATK = 250
+    DEF = 100
+
 user_class = input("Choose a class between Warrior, Mage, or Rouge\n")
 print(user_class)
-while(user_class is not 'Warrior' or user_class is not 'Mage' or user_class is not 'Rouge'):
+
+while(user_class != 'Warrior' and user_class != 'Mage' and user_class != 'Rouge'):
     print("You did not choose from the required classes, try again")
     user_class = input()
+
+
