cb0c1ceaed48229cf35b159d0351d9bbcfd4eced
voorXIII <demarkisgeorge1@gmail.com>
2019-04-16 14:09:59 -0400

    cleaned up code

--- TxtAdventureProject.py
+++ TxtAdventureProject.py
@@ -1,10 +1,7 @@
 import random  # Importing random
 
-min_roll = 1  # Min roll value
-max_roll = 10 # Max roll value
-
-
-
+min_roll = 1   # Min roll value
+max_roll = 10  # Max roll value
 
 print("Welcome to the game.\n")  # This welcomes you to the game.
 
@@ -17,20 +14,24 @@ class Warrior:  # The Warrior class values.
     HP = 10
     ATK = 10
     DEF = 10
-    Damage = random.randint(1,10)
+    Damage = random.randint(1, 10)
+
 
 class Mage:  # The Mage class values.
     HP = 10
     ATK = 10
     DEF = 5
-    Damage = random.randint(1,10)
+    Damage = random.randint(1, 10)
+
+
 class Rogue:  # The Rogue class values.
     HP = 10
     ATK = 10
     DEF = 8
-    Damage = random.randint(1,10)
+    Damage = random.randint(1, 10)
 
- class Skeleton:  # The Skeleton class values.
+
+class Skeleton:
     def __init__(self, Name, HP, ATK, DEF, Damage):
         self.Name = Name
         self.HP = HP
@@ -38,22 +39,24 @@ class Rogue:  # The Rogue class values.
         self.DEF = DEF
         self.Damage = Damage
 
-def attack(attacker,attackee):  # Attack function
-    hit = random.randint(min_roll,max_roll) + attacker.ATK
-     
-        if(hit > attackee.DEF):
-         print("The attack hits")
-        
-        attackee.HP = attackee.HP - attacker.Damage
-        
-        if attackee.HP <= 0:  # If the attackee's health drops below zero
-            print(attackee.Name "falls to the floor, dead.")
-            else:
-                print(attackee.Name "has" attackee.HP, "HP remaining.")
-            else:
-                print("You missed.")
+def attack(attacker, attackee):
+    hit = random.randint(min_roll, max_roll) + attacker.ATK
 
- user_class = input("Choose a class between Warrior, Mage, or Rogue.\n")   # Input what class you want to be.
+    if (hit > attackee.DEF):
+        print("It hits.")
+        print(attacker.Name, "inflicts", attacker.Damage)
+
+        attackee.HP = attackee.HP - attacker.Damage
+
+        if attackee.HP <= 0:  # if the attackee's health drops below zero
+            print("With one more attack,", attackee.Name, "falls to the floor, dead.")
+        else:
+            print(attackee.Name, "has", attackee.HP, "HP remaining.")
+    else:
+        print("You missed.")
+
+
+user_class = input("Choose a class between Warrior, Mage, or Rogue.\n")   # Input what class you want to be.
 
 while(user_class != 'Warrior' and user_class != 'Mage' and user_class != 'Rogue'):  # If you input another class other than the 3 specified, it gives that print statement and re-runs the class input.
     print("You did not choose from the required classes, try again.")
@@ -70,15 +73,19 @@ else:
     exit()
 
 
+print("This is your HP.", theClass.HP)  # Tells you your HP value
+print("This is your Attack.", theClass.ATK)  # Tells you your Attack value
+print("This is your Defense.", theClass.DEF)  # Tells you your Defense value
+
+enter = input("Please press Enter to continue.\n")
+
 print("You are now ready to play.\n")  # Tells the user that they are ready to play the game.
 
 enter = input("Please press Enter to continue.\n") # Press the enter key to continue the game.
 
 print("An old mage tells you to go to the Dungeon and kill a Skeleton to prove your worth.\n")  # NPC tells you to go kill a monster to prove your worth.
 
-skeleton1 = Skeleton("The Skeleton", 10,4,5,5)  # This creates a new Skeleton enemy. The order is the Name, HP, ATK, DEF, and Damage.
+skeleton1 = Skeleton("The Skeleton", 10, 4, 5, 5)  # This creates a new Skeleton enemy. The order is the Name, HP, ATK, DEF, and Damage.
 
-while skeleton1.HP > 0:
-    attack(theClass,skeleton1)
-print("It's dead. Return to the old mage."
+
 
